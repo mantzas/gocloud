fb60cbe8594b992015fbc9625b9311ec1fd7e058
Sotirios Mantziaris <smantziaris@gmail.com>
2018-03-17 22:45:16 +0200

    Pool metrics and default sanitizer (#3)

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -9,16 +9,3 @@ type Tag struct {
 func NewTag(k, v string) Tag {
 	return Tag{k, v}
 }
-
-// Counter interface
-type Counter interface {
-	Increase(value int, tags ...Tag)
-}
-
-// NullCounter defines a counter that does nothing
-type NullCounter struct {
-}
-
-// Increase the null counter
-func (nc NullCounter) Increase(value int, tags ...Tag) {
-}
